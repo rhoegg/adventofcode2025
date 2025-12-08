720da8856c2e0280cb5bb353ed57f5bb95806756
Ryan Hoegg <ryan.hoegg@gmail.com>
2025-12-07 22:20:53 -0600

    day 7 part 2 in c - tachyon timelines

--- c/day07/main.c
+++ c/day07/main.c
@@ -16,8 +16,11 @@ int main(int argc, char **argv) {
     
     const size_t manifold_width = strlen(lines[line_count - 1]);
     bool tachyons[manifold_width];
+    size_t tachyon_timelines[manifold_width];
+
     for (size_t i = 0; i < manifold_width; ++i) {
         tachyons[i] = (lines[0][i] == 'S');
+        tachyon_timelines[i] = (lines[0][i] == 'S');
     }
 
     size_t split_count = 0;
@@ -26,23 +29,33 @@ int main(int argc, char **argv) {
         for (size_t j = 0; j < manifold_width; ++j) {
             if (tachyons[j] && lines[i][j] == '^') {
                 tachyons[j] = false;
+                size_t timelines = tachyon_timelines[j];
+                tachyon_timelines[j] = 0;
                 ++split_count;
                 if (j > 0) {
                     tachyons[j - 1] = true;
+                    tachyon_timelines[j - 1] += timelines;
                 }
                 if (j < (manifold_width - 1)) {
                     tachyons[j + 1] = true;
+                    tachyon_timelines[j + 1] += timelines;
                 }
             }
         }
         for (size_t j = 0; j < manifold_width; j++) {
-            if (tachyons[j]) printf("|");
+            if (tachyons[j]) printf("%lu", tachyon_timelines[j] % 10);
             else printf("%c", lines[i][j]);
         }
         printf("\n");
     }
 
+    size_t timeline_count = 0;
+    for (size_t j = 0; j < manifold_width; j++) {
+        timeline_count += tachyon_timelines[j];
+    }
+
     printf("Part 1: %lu splits\n", split_count);
+    printf("Part 2: %lu timelines\n", timeline_count);
 
     free(buf);
 }
